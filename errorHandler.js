d417fc1cd20ad065989fb03b47aa44ba74e40133
Libikk <libikpl@gmail.com>
2020-01-31 06:52:19 +0000

    refactor: switch off debug mode

--- errorHandler.js
+++ errorHandler.js
@@ -4,7 +4,6 @@ import { SENTRY_PUBLIC_KEY, SENTRY_PROJECT_ID } from 'react-native-dotenv'
 Sentry.init({
     dsn: `https://${SENTRY_PUBLIC_KEY}@sentry.io/${SENTRY_PROJECT_ID}`,
     enableInExpoDevelopment: true,
-    debug: true,
 });
 
 export { Sentry };
