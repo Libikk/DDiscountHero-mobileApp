ecd18f6d7c4a4f5af88bb878a2639774518e452a
Libikk <libikpl@gmail.com>
2020-02-15 15:05:09 +0000

    refactor: remove unneeded stuff

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import 'react-native-gesture-handler';
-import React, { useState, useEffect, useContext, Children } from 'react';
-
+import React from 'react';
 import DefaultView from './components/DefaultView';
-import LoginPage from './screens/LoginPage';
-import Store, { UserDataContext } from './Store';
-import Home from './screens/Home';
-import ProductList from './screens/ProductList';
-import { Notifications } from 'expo';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { authorize } from './services/authorizeUtils';
+import Store from './Store';
 
 
 export default App = () => {
