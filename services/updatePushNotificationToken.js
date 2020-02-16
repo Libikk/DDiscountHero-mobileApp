5806ee415685d7e0aa3137c59c389848c0ef9954
Libikk <libikpl@gmail.com>
2020-02-16 11:24:50 +0000

    refactor: move update push notification token to services

--- services/updatePushNotificationToken.js
+++ services/updatePushNotificationToken.js
@@ -1,9 +1,7 @@
 import { Notifications } from 'expo';
 import axios from 'axios';
 import * as Permissions from 'expo-permissions';
-import appConfig from './appConfig';
-
-const PUSH_ENDPOINT = `${appConfig.ddiscountHeroUrl}/api/user/updateUserPushNotificationToken`;
+import appConfig from '../appConfig';
 
 const updatePushNotificationToken = async () => {
     const permissionsResponse = await Permissions.askAsync(Permissions.NOTIFICATIONS).catch(console.error);;
@@ -14,7 +12,7 @@ const updatePushNotificationToken = async () => {
     }
     
     let token = await Notifications.getExpoPushTokenAsync().catch(console.error);
-    return axios.post(PUSH_ENDPOINT, { token });
+    return axios.post(`${appConfig.ddiscountHeroUrl}/api/user/updateUserPushNotificationToken`, { token });
 }
 
 
