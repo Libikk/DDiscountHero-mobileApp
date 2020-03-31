c9c96b781c699fecf565fa82f0536c67cdb21637
Libikk <libikpl@gmail.com>
2020-03-31 06:57:32 +0100

    chore: navigate back to login page

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
-import { TextInput, View, Text, Image, AsyncStorage, Button } from 'react-native';
+import React from 'react';
+import { View, Text, Image, AsyncStorage, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
 
-const Home = (props) => (
-  <View style={{ padding: 30 }}>
-    {/* <Button onPress={() => AsyncStorage.removeItem('userToken')} title="temove token"></Button> */}
-    <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
-      <Image style={{ resizeMode: 'center' }} source={require('../assets/images/logo.png')} />
+const Home = (props) => {
+  const logout = () => {
+    AsyncStorage.removeItem('userToken');
+    props.navigation.navigate('LoginPage');
+  };
+  return (
+    <View style={{ padding: 30 }}>
+      <TouchableOpacity onPress={logout}>
+        <MaterialCommunityIcons name="logout" />
+      </TouchableOpacity>
+      <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <Image style={{ resizeMode: 'center' }} source={require('../assets/images/logo.png')} />
+      </View>
+      <View>
+        <Text style={{ marginTop: 30, textAlign: 'center', fontWeight: 'bold', fontSize: 18, lineHeight: 30 }}>
+          Discounts on products you love in one place 🚀
+        </Text>
+      </View>
     </View>
-    <View>
-      <Text style={{ marginTop: 30, textAlign: 'center', fontWeight: 'bold', fontSize: 18, lineHeight: 30 }}>
-        Discounts on products you love in one place 🚀
-      </Text>
-    </View>
-  </View>
-);
+  );
+};
 
 export default Home;
